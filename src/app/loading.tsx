49d5abb0ecd628e19771ee57063ef13b58a46149
Seth Wilson <sethwilson@Seths-Mac-Studio.local>
2024-06-03 05:43:15 -0500

    tweak loading message

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ const Loading = () => {
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div className="flex flex-col z-10 w-full max-w-5xl items-center justify-between lg:flex">
         <h1 className="text-4xl font-bold tracking-tight lg:text-5xl pr-2">
-          Hang tight... Fetching next poem...
+          Hang tight, finding you a great poem...
         </h1>
       </div>
     </main>
