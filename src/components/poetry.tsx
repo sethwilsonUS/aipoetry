aedb9731027f29c58e97c3cafe0029529437bd21
Seth Wilson <sethhwilson@gmail.com>
2024-06-08 19:45:01 -0500

    add a close button to the poem info popover

--- src/components/poetry.tsx
+++ src/components/poetry.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { PopoverTrigger, PopoverContent, Popover } from '@/components/ui/popover'
+import { PopoverTrigger, PopoverClose, PopoverContent, Popover } from '@/components/ui/popover'
 import { IPoetryComponentProps } from '@/types/poetry'
 
 const Poetry: React.FC<IPoetryComponentProps> = (props) => {
@@ -18,7 +18,14 @@ const Poetry: React.FC<IPoetryComponentProps> = (props) => {
             <PopoverContent className="w-80 ml-2">
               <div className="grid gap-4">
                 <div className="space-y-2">
-                  <h4 className="font-medium leading-none">{props.styleName}</h4>
+                  <div className='flex justify-between'>
+                    <h4 className="font-medium leading-none">{props.styleName}</h4>
+                    <PopoverClose asChild>
+                      <button className="rounded-sm">
+                        <XIcon />
+                      </button>
+                    </PopoverClose>
+                  </div>
                   <p className="text-sm text-gray-500 dark:text-gray-400">
                     {props.styleExplanation}
                   </p>
@@ -56,6 +63,27 @@ function InfoIcon(props: any) {
       <circle cx="12" cy="12" r="10" />
       <path d="M12 16v-4" />
       <path d="M12 8h.01" />
+    </svg>
+  )
+}
+
+function XIcon(props: any) {
+  return (
+    <svg
+      {...props}
+      className="w-4 h-4"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
     </svg>
   )
 }
