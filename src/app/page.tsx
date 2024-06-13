8fd628174958d17665c55847d667be9bd9c89777
Seth Wilson <sethhwilson@gmail.com>
2024-06-12 20:56:08 -0500

    write poems to supabase after generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,8 @@ import dynamic from 'next/dynamic'
 import getRandomPoem from '@/lib/getRandomPoem';
 import { IPoetry } from '@/types/poetry';
 
-import { getPoems } from '@/lib/supabase';
-
 import Poetry from '../components/poetry';
 import Countdown from '../components/countdown';
-
-// const Countdown = dynamic(() => import('../components/countdown'), { ssr: false});
 
 export default async function Home() {
   revalidatePath('/');
@@ -35,10 +31,7 @@ export default async function Home() {
   }
 
   const { title, lines, styleName, styleExplanation } = poetryRes;
-
-  const poems = await getPoems();
-  console.log(JSON.stringify(poems, null, 2));
-
+  
   return (
     <main className="flex flex-col items-center justify-between p-12">
       <div className="flex flex-col z-10 w-full max-w-4xl justify-between lg:flex">
